6458d0dc1276c8595ca974a3789c6f896ad6a239
noob9527 <2061864@qq.com>
2019-09-29 14:00:18 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = () => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setCounter(counter + 1), 1000);
+    return () => {
+      clearTimeout(timer);
+    };
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -18,9 +27,10 @@ const App: React.FC = () => {
         >
           Learn React
         </a>
+        <div>counter: {counter}</div>
       </header>
     </div>
   );
-}
+};
 
 export default App;
