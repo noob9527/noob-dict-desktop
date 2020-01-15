8cdc071fbdbd701b123309c73498cca89f7ee57b
noob9527 <2061864@qq.com>
2020-01-15 22:48:50 +0800

    update

--- src/browser/pages/search/note/history-table.tsx
+++ src/browser/pages/search/note/history-table.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { ISearchHistory } from '../../../../common/model/history';
 import styled from 'styled-components';
-import { Table } from 'antd';
-import moment from 'moment';
-import _ from 'lodash';
 
 interface HistoryViewProps {
   histories: ISearchHistory[]
@@ -17,31 +14,6 @@ const Container = styled.div`
 const HistoryTable: React.FC<HistoryViewProps> = (props) => {
   const { histories } = props;
 
-  const dataSource = _.chain(histories)
-    .sortBy(e => e.createAt)
-    .map((e, i) => {
-      const first = i === 0;
-      return {
-        ...e,
-        first,
-        createAtStr: first ? 'Current' : moment(e.createAt).format('YYYY-MM-DD HH:mm:ss'),
-        key: i,
-      };
-    }).value();
-
-  const columns = [
-    {
-      title: 'Time',
-      key: 'Time',
-      dataIndex: 'createAtStr',
-    },
-    {
-      title: 'Context',
-      key: 'Context',
-      dataIndex: 'context',
-    },
-  ];
-
   return (
     <>
       <Container>
@@ -53,10 +25,12 @@ const HistoryTable: React.FC<HistoryViewProps> = (props) => {
           </tr>
           </thead>
           <tbody>
-          <tr>
-            <td></td>
-            <td></td>
-          </tr>
+          {histories.map((e, i) => (
+            <tr key={i}>
+              <td>{new Date(e.createAt).toISOString()}</td>
+              <td>{e.context}</td>
+            </tr>
+          ))}
           </tbody>
         </table>
       </Container>
